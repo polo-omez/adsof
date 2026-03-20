ac25dd572e39df833fc0110df6c8871d75121947
polo-omez <pablogomez0904@gmail.com>
2026-03-20 22:07:52 +0100

    apartados 2,3 diagramas

--- p4/src/main/java/meteorologia/sensores/ISensor.java
+++ p4/src/main/java/meteorologia/sensores/ISensor.java
@@ -1,8 +1,12 @@
 package meteorologia.sensores;
 
+import java.time.LocalDateTime;
+
 public interface ISensor {
 
   public String getIdentificador();
+
+  public LocalDateTime getFechaInstalacion();
 
   public void medir();
 
--- p4/src/main/java/meteorologia/sensores/SensorMetereologico.java
+++ p4/src/main/java/meteorologia/sensores/SensorMetereologico.java
@@ -1,5 +1,41 @@
 package meteorologia.sensores;
 
+import java.time.LocalDateTime;
+
 public abstract class SensorMetereologico implements ISensor {
+  private String id;
+  private double offsetCalibracion;
+  private double ultimaLectura;
+  private LocalDateTime fechaUltimaLecutra;
+  private LocalDateTime fechaInstalacion;
+
+  public SensorMetereologico(String id, double offsetCalibracion) {
+    this.id = id;
+    this.offsetCalibracion = offsetCalibracion;
+    this.fechaUltimaLecutra = LocalDateTime.now();
+
+  }
+
+  @Override
+  public String getIdentificador() {
+    return this.id;
+  }
+
+  @Override
+  public LocalDateTime getFechaInstalacion() {
+    return fechaInstalacion;
+  }
+
+  @Override
+  public void calibrar(double offset) {
+    this.offsetCalibracion = offset;
+
+  }
+
+  @Override
+  public void medir() {
+    this.fechaUltimaLecutra = LocalDateTime.now();
+
+  }
 
 }
