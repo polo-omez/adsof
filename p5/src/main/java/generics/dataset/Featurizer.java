4356c93a83ad0fcb0cb56c18da25b3b2f7f40707
polo-omez <pablogomez0904@gmail.com>
2026-04-20 19:42:24 +0200

    asdf

--- p5/src/main/java/generics/dataset/Featurizer.java
+++ p5/src/main/java/generics/dataset/Featurizer.java
@@ -1,5 +1,11 @@
 package generics.dataset;
 
+import java.util.Set;
+
 public interface Featurizer {
+
+  public <E extends Comparable<E>> Feature<E> feature(String field);
+
+  public Set<String> freaturizeDataSet();
 
 }
